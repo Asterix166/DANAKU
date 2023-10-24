7b62f33067fac1f23b06eef5804d6b9e871f3405
Asterix166 <2111523006_fahri@student.unand.ac.id>
2023-10-24 21:07:35 +0700

    arip

---
+++ app/src/main/java/com/example/danaku/aripHOME.kt
@@ -0,0 +1,11 @@
+package com.example.danaku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class aripHOME : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arip_home)
+    }
+}
