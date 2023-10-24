f844797c3d377de2178152c3bfdbd981c99bb3dd
Asterix166 <2111523006_fahri@student.unand.ac.id>
2023-10-24 20:36:22 +0700

    p

---
+++ app/src/main/java/com/example/danaku/fahriHOME.kt
@@ -0,0 +1,11 @@
+package com.example.danaku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class fahriHOME : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fahri_home)
+    }
+}
